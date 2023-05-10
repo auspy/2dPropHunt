e32585e8b49a13be94f8522d5cf8d97088a93d97
RaushanBhanu <roushan.bhanu123@gmail.com>
2023-05-10 21:37:52 +0530

    Health Bar added

--- Assets/Script/Hunter.cs
+++ Assets/Script/Hunter.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Hunter : Character
 {
     // VARIABLES
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+    
     // movements
     bool isAttacking = false;
 
     // char animation states
     public int stateAttackSlash = 6;
     public int stateAttckKick = 7;
-
     // FUNCTIONS
     public Hunter()
     {
@@ -26,7 +30,7 @@ public class Hunter : Character
     {
         moveCharacter();
         CharacterAnimations();
-moveHunter();
+        moveHunter();
         AnimateAttackSlash();
         AnimateSlide();
     }
@@ -37,19 +41,32 @@ moveHunter();
         {
             print("attacking");
             isAttacking = true;
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                // Debug.Log("We hit "+enemy.name);
+                enemy.GetComponent<enemy>().TakeDamage(attackDamage);
+            }
         }
         else if (Input.GetKey(KeyCode.X) && isMoving && !isSliding)
         {
             print("sliding");
             isSliding = true;
-            rg.velocity =new Vector2(movX * (runSpeed), movY) ;
+            rg.velocity = new Vector2(movX * (runSpeed), movY);
         }
         else
         {
             isAttacking = false;
             isSliding = false;
         }
-        
+
+    }
+
+    void OnDrawGizmosSelected(){
+        if(attackPoint== null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position,attackRange);
     }
 
     void AnimateAttackSlash()
